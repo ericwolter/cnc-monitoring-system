118fa19472596bf133e9039f80e7186c9eecb0fd
Eric <502286+ericwolter@users.noreply.github.com>
2023-10-26 22:46:51 +0200

    Refactor dashboard for clarity and modularity
    - Consolidate CSS classes for chart containers for better maintainability.
    - Refactor JavaScript to introduce a MachineDashboard class for encapsulation.
    - Implement a logger proxy for detailed method call logging.
    - Improve error handling in the Kafka consumer.
    - Separate the message processing logic for better clarity.
    - Optimize web server initialization and shutdown procedures.
    - Update HTML templates to match the new JS structure.

--- src/cloud/dashboard/static/js/dashboard.js
+++ src/cloud/dashboard/static/js/dashboard.js
@@ -1,67 +1,229 @@
-window.activeMachine = null;
-
-const socket = io.connect('http://' + document.domain + ':' + location.port);
-
-socket.on('factory_machine_status', function (data) {
-    // Update the status of the specific machine
-    updateMachineStatus(data.machine_id, data.status);
-});
-
-socket.on('initial-status', function (data) {
-    // Handle the initial statuses here
-    for (let machine_id in data) {
-        updateMachineStatus(machine_id, data[machine_id]);
+function getVibrationChartConfig() {
+    return {
+        type: 'line',
+        data: {
+            datasets: [{
+                borderColor: "#07a889",
+                borderWidth: 1,
+                pointRadius: 0,
+                data: []
+            }]
+        },
+        options: {
+            animation: false,
+            maintainAspectRatio: false,
+            parsing: false,
+            normalized: true,
+            interaction: {
+                intersect: false
+            },
+            scales: {
+                x: {
+                    type: 'linear',
+                    min: 0,
+                    max: 50000
+                },
+                y: {
+                    min: -2500,
+                    max: 2500
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: 'Vibration Data'
+                }
+            }
+        }
     }
-    if (window.activeMachine) {
-        toggleMachineData(window.activeMachine);
+};
+
+class MachineDashboard {
+    constructor() {
+        this.activeMachine = null;
+        this.dashboardState = 'initialized';
+        this.socket = io.connect('http://' + window.location.hostname + ':' + location.port);
+        this.vibrationChartX = this.initVibrationChart("vibrationChartX");
+        this.vibrationChartY = this.initVibrationChart("vibrationChartY");
+        this.vibrationChartZ = this.initVibrationChart("vibrationChartZ");
+
+        this.vibrationCharts = [this.vibrationChartX, this.vibrationChartY, this.vibrationChartZ];
+
+        this.initSocketEvents();
     }
-});
 
-function toggleMachineData(machine_id) {
-    let machineElement = document.getElementById(machine_id);
-
-    // Remove 'active' class from all machines
-    let machines = document.querySelectorAll('.machine');
-    machines.forEach(function (machine) {
-        machine.classList.remove('active');
-    });
-
-    if (window.activeMachine && window.activeMachine === machine_id) {
-        window.activeMachine = null;  // If the machine is toggled off, set the activeMachine to null
-    } else {
-        window.activeMachine = machine_id;  // If a new machine is toggled on, set the activeMachine
-        machineElement.classList.add('active');
+    initVibrationChart(elementId) {
+        return new Chart(document.getElementById(elementId), getVibrationChartConfig());
     }
-    // Toggle placeholder visibility based on the active machine status
-    togglePlaceholderVisibility();
 
-    socket.emit('toggle-machine-data', { machine_id: machine_id });
-}
+    initSocketEvents() {
+        this.socket.on('connect', () => {
+            console.log('Connected to socket server.');
+            this.dashboardState = 'connected';
+            this.updateVisibility();
+        });
+        this.socket.on('disconnect', () => {
+            console.warn('Disconnected from socket server.');
+            this.dashboardState = 'disconnected';
+            this.highlightActiveMachine(null);
+            this.updateVisibility("Disconnected. Trying to reconnect...");
+        });
+        this.socket.on('error', error => {
+            console.error('Socket error:', error);
+            this.dashboardState = 'error';
+            this.updateVisibility("An error occurred. Please refresh or contact support.");
+        });
 
-function togglePlaceholderVisibility() {
-    let placeholder = document.querySelector('.chart-placeholder');
+        this.socket.on('factory_machine_status', data => this.updateMachineStatus(data.machine_id, data.status));
+        this.socket.on('initial-status', data => this.initializeAllMachineStatuses(data));
+        this.socket.on('start-chart', () => this.startCharts());
+        this.socket.on('stop-chart', () => this.stopCharts());
+        this.socket.on('update-chart', data => this.updateCharts(data));
+    }
 
-    // If no chart is visible and there's no active machine, show the placeholder
-    if (!window.activeMachine) {
+    initializeAllMachineStatuses(data) {
+        for (let machine_id in data) {
+            this.updateMachineStatus(machine_id, data[machine_id]);
+        }
+        if (this.activeMachine) {
+            this.handleMachineSelection(this.activeMachine);
+        }
+    }
+
+    updateMachineStatus(machine_id, status) {
+        console.log(`[MachineDashboard] machine '${machine_id}' state change to '${status}'`);
+        let machineElement = document.getElementById(machine_id);
+        if (machineElement) {
+            let statusPill = machineElement.querySelector('.status-pill');
+            statusPill.textContent = status;
+
+            this.setMachineAppearance(machineElement, status);
+
+            if (status === "Starting" && machine_id === this.activeMachine) {
+                this.clearCharts();
+            }
+        }
+    }
+
+    startCharts() {
+        this.clearCharts();
+
+        this.updateVisibility();
+    }
+
+    stopCharts() {
+        this.clearCharts();
+        this.updateVisibility();
+    }
+
+    updateCharts(data) {
+        const targetMachineId = this.activeMachine === null ? data.machine_id : this.activeMachine;
+        if (data.machine_id !== targetMachineId) {
+            this.socket.emit('toggle-machine-data', { machine_id: targetMachineId });
+        }
+
+        if (data.sequence_start === 0) {
+            this.clearCharts();
+        }
+
+        for (let i = 0; i < data.vibration_data.length; i++) {
+            let xyz = data.vibration_data[i];
+            let j = data.sequence_start + i;
+
+            this.vibrationCharts.forEach((chart, index) => {
+                chart.data.datasets[0].data.push({ x: j, y: xyz[index] });
+            });
+        }
+
+        this.vibrationCharts.forEach(chart => {
+            chart.update();
+        });
+    }
+
+    updateVisibility(message) {
+        if (this.dashboardState === 'connected' && this.activeMachine) {
+            this.showCharts();
+            this.hidePlaceholder();
+        } else {
+            this.hideCharts();
+            this.showPlaceholder(message || "Please select a machine to view its vibration data.");
+        }
+    }
+
+    showCharts() {
+        const titles = ['Vibration X for ', 'Vibration Y for ', 'Vibration Z for '];
+
+        this.vibrationCharts.forEach((chart, index) => {
+            chart.canvas.style.visibility = "visible";
+            chart.options.plugins.title.text = titles[index] + this.activeMachine;
+            chart.update();
+        });
+    }
+
+    hideCharts() {
+        this.vibrationCharts.forEach(chart => {
+            chart.canvas.style.visibility = "hidden";
+        });
+    }
+
+    showPlaceholder(message) {
+        let placeholder = document.getElementById('chart-placeholder');
+        placeholder.textContent = message;
         placeholder.style.display = "block";
-    } else {
+    }
+
+    hidePlaceholder() {
+        let placeholder = document.getElementById('chart-placeholder');
         placeholder.style.display = "none";
     }
-}
 
-function updateMachineStatus(machine_id, status) {
-    // Find the specific machine element by its ID
-    let machineElement = document.getElementById(machine_id);
-    if (machineElement) {
-        // Get the image and status pill elements
+    handleMachineSelection(machine_id) {
+        if (this.dashboardState !== 'connected') {
+            return
+        }
+
+        this.highlightActiveMachine(machine_id);
+        try {
+            this.socket.emit('toggle-machine-data', { machine_id: machine_id });
+        } catch (error) {
+            console.error('Error while toggling machine data:', error);
+        }
+        this.updateVisibility();
+    }
+
+    highlightActiveMachine(machine_id) {
+        let machineElement = machine_id ? document.getElementById(machine_id) : null;
+
+        let machines = document.querySelectorAll('.machine');
+        machines.forEach(function (machine) {
+            machine.classList.remove('active');
+        });
+
+        let prevState = this.activeMachine;  // Save the previous state
+
+        if (!machine_id) {
+            this.activeMachine = null;
+        }
+        else if (this.activeMachine && this.activeMachine === machine_id) {
+            this.activeMachine = null;  // If the machine is toggled off, set the activeMachine to null
+        } else {
+            this.activeMachine = machine_id;  // If a new machine is toggled on, set the activeMachine
+            machineElement.classList.add('active');
+        }
+
+        console.log(`[MachineDashboard] activeMachine changed from '${prevState}' to '${this.activeMachine}'`);
+    }
+
+    setMachineAppearance(machineElement, status) {
         let imageElement = machineElement.querySelector('img');
         let statusPill = machineElement.querySelector('.status-pill');
 
-        // Set status text
-        statusPill.textContent = status;
-
         machineElement.classList.remove('inspection');
         machineElement.classList.remove('maintenance');
+
         // Set the image and pill color based on status
         switch (status) {
             case "Initializing":
@@ -97,180 +259,37 @@ function updateMachineStatus(machine_id, status) {
                 statusPill.style.backgroundColor = "gray";
                 break;
         }
+    }
 
-        if (status === "Starting" && machine_id === window.activeMachine) {
-            vibrationChartX.data.datasets[0].data = [];
-            vibrationChartY.data.datasets[0].data = [];
-            vibrationChartZ.data.datasets[0].data = [];
-            vibrationChartX.update();
-            vibrationChartY.update();
-            vibrationChartZ.update();
-        }
+    clearCharts() {
+        this.vibrationCharts.forEach(chart => {
+            chart.data.datasets[0].data = [];
+            chart.update();
+        });
     }
 }
 
-socket.on('start-chart', function () {
-    vibrationChartX.data.datasets[0].data = [];
-    vibrationChartY.data.datasets[0].data = [];
-    vibrationChartZ.data.datasets[0].data = [];
-    vibrationChartX.options.plugins.title.text = 'Vibration X for ' + window.activeMachine;
-    vibrationChartY.options.plugins.title.text = 'Vibration Y for ' + window.activeMachine;
-    vibrationChartZ.options.plugins.title.text = 'Vibration Z for ' + window.activeMachine;
+// const dashboard = new MachineDashboard();
 
-    vibrationChartX.update();
-    vibrationChartY.update();
-    vibrationChartZ.update();
-
-    document.getElementById("vibrationChartX").style.visibility = "visible";
-    document.getElementById("vibrationChartY").style.visibility = "visible";
-    document.getElementById("vibrationChartZ").style.visibility = "visible";
-});
-
-socket.on('stop-chart', function () {
-    vibrationChartX.data.datasets[0].data = [];
-    vibrationChartY.data.datasets[0].data = [];
-    vibrationChartZ.data.datasets[0].data = [];
-
-    document.getElementById("vibrationChartX").style.visibility = "hidden";
-    document.getElementById("vibrationChartY").style.visibility = "hidden";
-    document.getElementById("vibrationChartZ").style.visibility = "hidden";
-});
-
-socket.on('update-chart', function (data) {
-    if (window.activeMachine === null) {
-        socket.emit('toggle-machine-data', { machine_id: data.machine_id });
-    } else {
-        if (data.machine_id !== window.activeMachine) {
-            socket.emit('toggle-machine-data', { machine_id: window.activeMachine });
+const logger = className => {
+    return new Proxy(new className(), {
+        get: function (target, name, receiver) {
+            if (!target.hasOwnProperty(name)) {
+                if (typeof target[name] === "function") {
+                    console.log(
+                        "Calling Method : ",
+                        name,
+                        "|| on : ",
+                        target.constructor.name
+                    );
+                }
+                return new Proxy(target[name], this);
+            }
+            return Reflect.get(target, name, receiver);
         }
-    }
+    });
+};
 
-    for (let i = 0; i < data.vibration_data.length; i++) {
-        xyz = data.vibration_data[i];
-        j = data.sequence_start + i
-        vibrationChartX.data.datasets[0].data.push({ x: j, y: xyz[0] });
-        vibrationChartY.data.datasets[0].data.push({ x: j, y: xyz[1] });
-        vibrationChartZ.data.datasets[0].data.push({ x: j, y: xyz[2] });
-    }
-    vibrationChartX.update();
-    vibrationChartY.update();
-    vibrationChartZ.update();
-});
 
-let vibrationChartX = new Chart(document.getElementById("vibrationChartX"), {
-    type: 'line',
-    data: {
-        datasets: [{
-            borderColor: "#07a889",
-            borderWidth: 1,
-            pointRadius: 0, // disable for a single dataset
-            data: []
-        }]
-    }, options: {
-        animation: false,
-        maintainAspectRatio: false,
-        parsing: false,
-        normalized: true,
-        interaction: {
-            intersect: false
-        },
-        scales: {
-            x: {
-                type: 'linear',
-                min: 0,
-                max: 50000,
-            },
-            y: {
-                min: -2500,
-                max: 2500,
-            }
-        },
-        plugins: {
-            legend: {
-                display: false,
-            },
-            title: {
-                display: true,
-                text: 'Vibration Data',
-            }
-        }
-    }
-});
-let vibrationChartY = new Chart(document.getElementById("vibrationChartY"), {
-    type: 'line',
-    data: {
-        datasets: [{
-            borderColor: "#07a889",
-            borderWidth: 1,
-            pointRadius: 0, // disable for a single dataset
-            data: []
-        }]
-    }, options: {
-        animation: false,
-        maintainAspectRatio: false,
-        parsing: false,
-        normalized: true,
-        interaction: {
-            intersect: false
-        },
-        scales: {
-            x: {
-                type: 'linear',
-                min: 0,
-                max: 50000,
-            },
-            y: {
-                min: -2500,
-                max: 2500,
-            }
-        },
-        plugins: {
-            legend: {
-                display: false,
-            },
-            title: {
-                display: true,
-                text: 'Vibration Data',
-            }
-        }
-    }
-});
-let vibrationChartZ = new Chart(document.getElementById("vibrationChartZ"), {
-    type: 'line',
-    data: {
-        datasets: [{
-            borderColor: "#07a889",
-            borderWidth: 1,
-            pointRadius: 0, // disable for a single dataset
-            data: []
-        }]
-    }, options: {
-        animation: false,
-        maintainAspectRatio: false,
-        parsing: false,
-        normalized: true,
-        interaction: {
-            intersect: false
-        },
-        scales: {
-            x: {
-                type: 'linear',
-                min: 0,
-                max: 50000,
-            },
-            y: {
-                min: -2500,
-                max: 2500,
-            }
-        },
-        plugins: {
-            legend: {
-                display: false,
-            },
-            title: {
-                display: true,
-                text: 'Vibration Data',
-            }
-        }
-    }
-});
+
+const dashboard = logger(MachineDashboard)
--- src/cloud/dashboard/webserver.py
+++ src/cloud/dashboard/webserver.py
@@ -7,13 +7,15 @@ from confluent_kafka import Consumer, KafkaError
 
 import sys
 
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 # Constants
 KAFKA_BROKER = "kafka:9092"
 KAFKA_STATUS_TOPIC = "factory_machine_status"
-
-# Set up logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
-logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
@@ -30,7 +32,7 @@ current_toggled_machine = None
 
 def kafka_consumer():
     try:
-        logger.info("Kafka consumer connecting...")
+        logger.info("Initializing Kafka consumer...")
         conf = {
             "bootstrap.servers": KAFKA_BROKER,
             "group.id": "dashboard-consumer-12345",
@@ -39,60 +41,59 @@ def kafka_consumer():
         }
 
         consumer = Consumer(conf)
-        logger.info(f"Subscribing to {KAFKA_STATUS_TOPIC} topic")
-        consumer.subscribe(
-            [
-                KAFKA_STATUS_TOPIC,
-                "factory_machine_M01_data",
-                "factory_machine_M02_data",
-                "factory_machine_M03_data",
-            ]
-        )
+
+        logging.info("Subscribing to Kafka topics...")
+        topics = [
+            KAFKA_STATUS_TOPIC,
+            "factory_machine_M01_data",
+            "factory_machine_M02_data",
+            "factory_machine_M03_data",
+        ]
+        consumer.subscribe(topics)
+        logging.info(f"Subscribed to topics: {', '.join(topics)}")
+        logger.info("Kafka consumer initialized and subscribed.")
 
         while True:
             message = consumer.poll(0.5)
             if message is None:
                 continue
             if message.error():
-                if message.error().code() == KafkaError._PARTITION_EOF:
-                    logger.info(
-                        "Reached end of %s [%d] at offset %d",
-                        message.topic(),
-                        message.partition(),
-                        message.offset(),
-                    )
-                else:
-                    logger.error(f"Kafka error: {message.error()}")
-                    sys.exit(-2)
+                logger.error(
+                    f"Kafka error on topic {message.topic()}: {message.error()}"
+                )
+                if message.error().code() != KafkaError._PARTITION_EOF:
+                    raise SystemExit("Critical Kafka error. Exiting application...")
             else:
-                if message.topic().startswith(KAFKA_STATUS_TOPIC):
-                    msg = message.value().decode("utf-8")
-                    machine_status = json.loads(msg)
-
-                    logger.info(f"Received machine status: {machine_status}")
-
-                    # Update the machines_status dictionary with the new status
-                    machine_id = machine_status["machine_id"]
-                    status = machine_status["status"]
-                    machines_status[machine_id] = status
-
-                    socketio.emit(f"{KAFKA_STATUS_TOPIC}", machine_status)
-                else:
-                    msg = message.value().decode("utf-8")
-                    machine_data = json.loads(msg)
-                    # Only emit if the machine is currently toggled on
-                    if (
-                        current_toggled_machine
-                        and current_toggled_machine == machine_data["machine_id"]
-                    ):
-                        logger.info(
-                            f"Received vibration data for toggled-on machine {machine_data['machine_id']}"
-                        )
-                        socketio.emit("update-chart", machine_data)
+                process_message(message)
 
     except Exception as e:
-        logger.error(f"consumer error: {e}")
-        sys.exit(-1)
+        logger.critical(f"Fatal error in Kafka consumer: {e}")
+        raise SystemExit("Exiting due to fatal error in Kafka consumer...")
+
+
+def process_message(message):
+    try:
+        if message.topic().startswith(KAFKA_STATUS_TOPIC):
+            msg = message.value().decode("utf-8")
+            machine_status = json.loads(msg)
+            machine_id = machine_status["machine_id"]
+            status = machine_status["status"]
+            machines_status[machine_id] = status
+            logger.info(f"Processed status for machine {machine_id}: {status}")
+            socketio.emit(f"{KAFKA_STATUS_TOPIC}", machine_status)
+        else:
+            msg = message.value().decode("utf-8")
+            machine_data = json.loads(msg)
+            machine_id = machine_data["machine_id"]
+            if current_toggled_machine and current_toggled_machine == machine_id:
+                logger.info(f"Processed vibration data for machine {machine_id}")
+                socketio.emit("update-chart", machine_data)
+    except json.JSONDecodeError:
+        logger.error(
+            f"Failed to decode JSON message from topic {message.topic()}: {message.value().decode('utf-8')}"
+        )
+    except Exception as e:
+        logger.error(f"Error processing message from topic {message.topic()}: {e}")
 
 
 @socketio.on("toggle-machine-data")
@@ -120,8 +121,6 @@ def handle_toggle(data):
 @socketio.on("connect")
 def handle_connection():
     logger.info("Client connected.")
-
-    # Emit the cached machine statuses to the newly connected client
     socketio.emit("initial-status", machines_status)
 
 
@@ -130,11 +129,18 @@ def index():
     return render_template("index.html", machines_status=machines_status)
 
 
+def main():
+    logger.info("Starting web server with integrated Kafka consumer...")
+    socketio.start_background_task(kafka_consumer)
+    socketio.run(app, host="0.0.0.0", port=5050, allow_unsafe_werkzeug=True)
+
+
 if __name__ == "__main__":
     try:
-        logger.info("Starting Kafka consumer...")
-        socketio.start_background_task(kafka_consumer)
-        logger.info("Starting webserver...")
-        socketio.run(app, host="0.0.0.0", port=5050, allow_unsafe_werkzeug=True)
+        main()
+    except SystemExit as se:
+        logging.error(se)
+        raise
     except Exception as e:
-        logger.error(f"server error: {e}")
+        logging.critical(f"Uncaught exception: {e}")
+        raise
