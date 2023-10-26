16f725cbe0b3d1b75d445ff86af1571faba54571
Eric <502286+ericwolter@users.noreply.github.com>
2023-10-26 20:01:58 +0200

    Enhance MQTT to Kafka connector with improved error handling and logging
    - Improved logging format to include log level for better traceability.
    - Introduced a separate function (`configure_kafka`) for Kafka configuration setup.
    - Enhanced MQTT connection handling by checking for connection return code.
    - Streamlined the topic subscription process and improved logging.
    - Added detailed error handling for Kafka exceptions during message processing.
    - Refactored main execution logic for better clarity and error handling.

--- src/cloud/mqtt-kafka-connector/mqtt_kafka_connector.py
+++ src/cloud/mqtt-kafka-connector/mqtt_kafka_connector.py
@@ -1,71 +1,91 @@
 import logging
-
-import paho.mqtt.client as mqtt
 import socket
-from confluent_kafka import Producer
+from confluent_kafka import Producer, KafkaException
+import paho.mqtt.client as mqtt
 
 # Set up logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-# Kafka configuration
+# Constants
 KAFKA_BROKER = "kafka:9092"
-KAKFA_CLIENT_ID = socket.gethostname()
-# KAFKA_DATA_TOPIC = "factory-data"
-# KAFKA_STATUS_TOPIC = "factory-status"
-
-# MQTT configuration
+KAFKA_CLIENT_ID = socket.gethostname()
 MQTT_BROKER_HOST = "mosquitto"
 MQTT_BROKER_PORT = 1883
 MQTT_DATA_TOPIC_PREFIX = "factory/machines"
 MQTT_STATUS_TOPIC = "factory/machines/status"
 
-logging.info(
-    f"Connecting to Kafka Broker {KAFKA_BROKER} with client.id {KAKFA_CLIENT_ID}..."
-)
-conf = {"bootstrap.servers": KAFKA_BROKER, "client.id": KAKFA_CLIENT_ID}
-producer = Producer(conf)
+
+def configure_kafka():
+    logging.info("Initializing Kafka configuration...")
+    conf = {"bootstrap.servers": KAFKA_BROKER, "client.id": KAFKA_CLIENT_ID}
+    logging.info("Kafka configuration initialized.")
+    return Producer(conf)
 
 
 def on_connect(client, userdata, flags, rc):
-    logging.info(
-        f"Connected to MQTT broker. Subscribing to topics: {MQTT_DATA_TOPIC_PREFIX}/M0x/data, {MQTT_STATUS_TOPIC}"
-    )
-    client.subscribe(
-        [
-            (MQTT_STATUS_TOPIC, 0),
-            (f"{MQTT_DATA_TOPIC_PREFIX}/M01/data", 1),
-            (f"{MQTT_DATA_TOPIC_PREFIX}/M02/data", 1),
-            (f"{MQTT_DATA_TOPIC_PREFIX}/M03/data", 1),
-        ]
-    )
+    if rc != 0:
+        logging.error(f"Failed to connect to MQTT broker. Return code: {rc}")
+        raise SystemExit(f"Fatal error: MQTT connection failed with return code {rc}")
+
+    topics = [
+        (MQTT_STATUS_TOPIC, 0),
+        (f"{MQTT_DATA_TOPIC_PREFIX}/M01/data", 1),
+        (f"{MQTT_DATA_TOPIC_PREFIX}/M02/data", 1),
+        (f"{MQTT_DATA_TOPIC_PREFIX}/M03/data", 1),
+    ]
+    logging.info("Subscribing to MQTT topics...")
+    client.subscribe(topics)
+    logging.info(f"Subscribed to topics: {', '.join([topic[0] for topic in topics])}")
 
 
 def on_message(client, userdata, message):
-    # Determine Kafka topic based on the incoming MQTT topic
-    if "status" in message.topic:
-        kafka_topic = f"factory_machine_status"
-    else:
-        machine_name = message.topic.split("/")[2]
-        kafka_topic = f"factory_machine_{machine_name}_data"
-
     try:
-        # Send the MQTT message to Kafka
+        if "status" in message.topic:
+            kafka_topic = "factory_machine_status"
+        else:
+            machine_name = message.topic.split("/")[2]
+            kafka_topic = f"factory_machine_{machine_name}_data"
+
         producer.produce(kafka_topic, message.payload)
+        logging.debug(
+            f"Forwarded message from {message.topic} to Kafka topic {kafka_topic}"
+        )
+
+    except KafkaException as ke:
+        logging.error(
+            f"Kafka error while processing message from {message.topic}: {ke}"
+        )
+        raise SystemExit("Fatal Kafka error. Exiting application...")
+
     except Exception as e:
         logging.error(
-            f"Local producer queue is full ({len(producer)} messages awaiting delivery): try again"
+            f"Unexpected error while processing message from {message.topic}: {e}"
         )
-        logging.error(f"Error: {e}")
+        raise SystemExit("Fatal unexpected error. Exiting application...")
 
 
-if __name__ == "__main__":
+def main():
     logging.info("Starting MQTT to Kafka connector...")
+
+    global producer
+    producer = configure_kafka()
 
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-
     client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
-
-    # Block and process MQTT messages
+    logging.info("Connected to MQTT broker. Entering message processing loop...")
     client.loop_forever()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit as se:
+        logging.error(se)
+        raise
+    except Exception as e:
+        logging.critical(f"Uncaught exception: {e}")
+        raise
